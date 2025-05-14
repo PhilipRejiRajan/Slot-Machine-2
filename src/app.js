baff6403841dd0abb7250ca2d31cc94b98fb5b10
Philip Reji Rajan <philiprejirajan@gmail.com>
2025-05-14 19:08:16 +0530

    Add Spin functions.

--- src/app.js
+++ src/app.js
@@ -31,3 +31,54 @@ depositBtn.addEventListener("click", () => {
     messageDisplay.innerText = "Deposit successful!";
     depositInput.value = "";
 });
+
+spinBtn.addEventListener("click", () => {
+    const lines = parseInt(linesInput.value);
+    const bet = parseFloat(betInput.value);
+    const totalBet = bet * lines;
+
+    if (isNaN(lines) || lines < 1 || lines > 3) {
+        messageDisplay.innerText = "Please enter a valid number of lines(1-3).";
+        return;
+    }
+
+    if (isNaN(bet) || bet <= 0) {
+        messageDisplay.innerText = "Please enter a valid bet per line";
+        return;
+    }
+
+    if(totalBet > balance){
+        messageDisplay.innerText = `Not enough balance. You need $${totalBet.toFixed(2)}`;
+        return;
+    }
+
+    const reels = spin();
+    const rows = transpose(reels);
+
+    displaySlotGrid(rows);
+
+    const winnings = getWinnings(rows, bet, lines);
+    balance -= totalBet;
+    balance += winnings;
+
+    balanceDisplay.innerText = balance.toFixed(2);
+    winningsDisplay.innerText = winnings.toFixed(2);
+
+    messageDisplay.innerText = winnings > 0
+        ? `You won $${winnings.toFixed(2)}!`
+        : "No win this time :(";
+
+    betInput.value = "";
+});
+
+function displaySlotGrid(rows) {
+    slotMachineDisplay.innerHTML = "";
+
+    for (let row of rows) {
+        for (let symbol of row) {
+            const cell = document.createElement("div");
+            cell.innerText = symbol;
+            slotMachineDisplay.appendChild(cell);
+        }
+    }
+}
